8876daf7d63508a8084b29ee68b94ce4d97376db
Oceanet31 <oceane.toto.lemene@gmail.com>
2025-12-17 17:33:52 +0100

    class Monster

---
+++ com/esiea/pootp/monsters/EarthMonster.java
@@ -0,0 +1,18 @@
+package com.esiea.pootp.monsters;
+
+public class EarthMonster extends Monster{
+
+    public EarthMonster(String name, int hp, int defense, double attack, int speed){
+        super(name, ElementType.EARTH, hp, defense, attack, speed);
+
+
+    }
+
+    public void attack(Monster monster){
+        
+    }
+
+    public void getAttacked(Monster monster){
+        
+    }
+}
